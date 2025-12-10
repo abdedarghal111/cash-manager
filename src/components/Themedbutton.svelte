1e10d2ac284058f23d05532031b0ea392d375b07
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2025-12-10 10:54:59 +0100

    feat(front): added enabled propiety

--- src/components/Themedbutton.svelte
+++ src/components/Themedbutton.svelte
@@ -2,12 +2,14 @@
 Componente que es un botón simple
 -->
 <script lang="ts">
-    let { label = "", onclick } = $props()
+    let { label = "", onclick, extraClass = '', enabled = true } = $props()
 </script>
 
-<button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white py-1.5 px-2.5 rounded"
+<button 
+    class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white py-1.5 px-2.5 rounded {extraClass}"
     type="button"
     onclick={onclick}
+    disabled={!enabled}
 >
     {label}
 </button>
