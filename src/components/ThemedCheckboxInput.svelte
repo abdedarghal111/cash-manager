99322ce02733b1dc8329559d381598d1ee48025e
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2026-02-01 16:56:12 +0100

    feat(front): nuevo componente

---
+++ src/components/ThemedCheckboxInput.svelte
@@ -0,0 +1,35 @@
+<!--
+Componente que es un checkbox de formulario siendo: ActivadoODesactivado NombreCampo
+
+Sus propiedades se controlan desde fuera, también el tipo.
+-->
+<script lang="ts">
+    let { label, value = $bindable(), nameBefore = false, disabled = false } = $props()
+
+    if (typeof value !== 'boolean') {
+        // revisar que siempre se introduzca un valor correcto
+        throw new Error('FATAL: Invalid ThemedCheckboxInput value', {
+            cause: `Se intentó introducir un valor "${value}" en un componente tipo checkbox que solo acepta booleanos.`
+        })
+    }
+</script>
+
+<div class="md:flex md:items-center">
+    <label class={`block text-blue-900 font-bold md:text-right mb-1 md:mb-0 pr-4 order-${nameBefore ? 1 : 2}`} for="inline-full-name">
+        {label}
+    </label>
+    <button class={
+    `order-${nameBefore ? 2 : 1} ${
+        value ? `bg-blue-500 hover:bg-blue-600`
+        : `bg-red-500 hover:bg-red-600`
+    } transition-all shadow focus:shadow-outline focus:outline-none text-white py-1.5 px-2.5 rounded`
+    } onclick={() => {
+        value = !value
+    }}>
+        {#if value === true}
+            Si
+        {:else}
+            No
+        {/if}
+    </button>
+</div>
