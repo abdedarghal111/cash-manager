64bd0706864f3a3921046befe034651f721221aa
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2026-02-16 15:58:39 +0100

    feat(component): text area input

---
+++ src/components/ThemedTextArea.svelte
@@ -0,0 +1,30 @@
+<!--
+Componente que es un textarea estilizado.
+-->
+<script lang="ts">
+    let { 
+        label,
+        value = $bindable(),
+        placeholder = "",
+        disabled = false
+    } = $props()
+</script>
+
+<div class="flex flex-col md:flex-row">
+    <div class="md:w-1/3">
+        <!-- svelte-ignore a11y_label_has_associated_control -->
+        <label class="text-blue-900 font-bold mb-1 md:mb-0 pr-4">
+            {label}
+        </label>
+    </div>
+    <div class="md:w-2/3">
+        <textarea
+            class="bg-gray-200 appearance-none w-full border-2 
+            border-gray-200 rounded py-2 px-2 text-gray-700 leading-tight 
+            focus:outline-none focus:bg-white focus:border-purple-500 resize-y"
+            bind:value={value}
+            {placeholder}
+            {disabled}
+        ></textarea>
+    </div>
+</div>
